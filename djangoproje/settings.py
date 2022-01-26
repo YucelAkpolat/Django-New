423009d5c5e4d5f8bd60fad471e2bb5ea4a8ed9d
darkk_bluee <yucelakpolat@gmail.com>
2022-01-26 09:51:13 +0300

    first commit

--- djangoproje/settings.py
+++ djangoproje/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangoproje.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresgl_psycopg2',
+        'NAME':  'mydb',
+        'USER':  'darkk_bluee',
+        'PASSWORD':  '6522683OWEN',
+        'HOST':  'db.sqlite3',
+        'PORT':  'db.sqlite3',
     }
 }
 
@@ -124,6 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')),] # yeni satır
+STATIC_ROOT = os.path.join(BASE_DIR,'static') # yeni sa
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') # yeni sa
